949ab115c33a8c23cbe7d9af70adf731f06daf3b
mati1yas <matiyassseifu@gmail.com>
2024-10-08 18:21:23 +0300

    feat : implement all features (import , process and export)

--- budget_allocation_tool/routes/api.php
+++ budget_allocation_tool/routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return "from api";
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
--- budget_allocation_tool/routes/web.php
+++ budget_allocation_tool/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ExcelController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/upload', function () {
     return view('welcome');
 });
+
+Route::post('/import-excel', [ExcelController::class,'importExcel']);
