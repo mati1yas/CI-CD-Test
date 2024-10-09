2599ddbfc48d7726d225268c52277efb53e6e38d
mati1yas <matiyassseifu@gmail.com>
2024-10-09 12:50:15 +0300

    feat : add routes and redirections

--- budget_allocation_tool/routes/api.php
+++ budget_allocation_tool/routes/api.php
@@ -1,25 +1,21 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ExcelController;
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-
 Route::get('/', function () {
-    return "from api";
+    return "aah staging api working fine";
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
+Route::post('/import-excel', [ExcelController::class,'importExcel']);
