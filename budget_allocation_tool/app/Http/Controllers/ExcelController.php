9916733f01739b7d9c5649876e452b07be408e41
mati1yas <matiyassseifu@gmail.com>
2024-10-09 12:51:10 +0300

    feat : add user input validation

--- budget_allocation_tool/app/Http/Controllers/ExcelController.php
+++ budget_allocation_tool/app/Http/Controllers/ExcelController.php
@@ -10,62 +10,68 @@ use App\Exports\EmployeeExport;
 use App\Imports\BudgetAllocationImport;
 use App\Imports\SpecificSheetsImport;
 use App\Services\BudgetAllocationService;
+use Illuminate\Support\Facades\Storage;
+
 
 class ExcelController extends Controller
 {
     // Method to import Excel data
     public function importExcel(Request $request)
-    {
+    {   
+
+      
         // Validate that a file has been uploaded
         $request->validate([
             'excel_file' => 'required|mimes:xlsx',
+            'date_picker'=> "required",
+            "document_number"=>"required",
+            "external_doc_reference"=>"required"
         ]);
-
-
+        
+        
         $sheetNames = ['Processed', 'GL Acct Lookup']; // Replace with your specific sheet names
 
         $file = $request->file('excel_file');
         $submission_date=  $request["date_picker"];
         $doc_number= $request["document_number"];
         $external_doc_reference= $request["external_doc_reference"];
-
-       
-
-
+        
+        
+        
+        
         // Import the Excel file, passing the list of sheets to the import class
         // Excel::import(new SpecificSheetsImport($sheetNames), $file);
         // Get the file from the request
-       
-
+        
+        
         // Import the EMPLOYEE RELATED Excel file
         $employeeImport = new DataImport("Payroll Data");
         Excel::import($employeeImport, $file);
         $employees = $employeeImport->employees;
-      
-
+        
+        
         
         
         // return $employees;
         // return $employees;
-
+        
         $budgetAllocationImport = new BudgetAllocationImport("Input-2 (LoE)");
         Excel::import($budgetAllocationImport, $file);
         $fundData = $budgetAllocationImport->fundData;
-
-
+        
+        
         // return $fundData;
-
+        
         $service = new BudgetAllocationService();
         $processedData = $service->distributePayments($employees, $fundData,$submission_date,$doc_number,$external_doc_reference);
+        // return $processedData;
         $export = new EmployeeExport($processedData);
 
-        // Download the new Excel file
+        
         return Excel::download($export, 'distributed_salaries.csv');  //xlsx
-        return $processedData;
-        return response()->json(['message' => 'File imported successfully!']);
     }
-
-
+    
+    
     // Method to export data to Excel
     public function export()
     {
