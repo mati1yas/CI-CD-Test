5c69437103a9529e9a2eeeae60cd572d48af789a
mati1yas <matiyassseifu@gmail.com>
2024-11-18 09:04:42 +0300

    fix : add a method to handle tax declaration form generation

--- budget_allocation_tool/app/Http/Controllers/ExcelController.php
+++ budget_allocation_tool/app/Http/Controllers/ExcelController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Imports\DataImport; // Replace with your actual import class
+use App\Imports\EmployeeDataImportForTaxTemplate;
 use App\Exports\DataExport; // Replace with your actual export class
 use App\Exports\EmployeeExport;
 use App\Imports\BudgetAllocationImport;
@@ -61,26 +62,52 @@ class ExcelController extends Controller
         
         
         
-        // return $employees;
-        // return $employees;
         
-        $budgetAllocationImport = new BudgetAllocationImport("Input-2 (LoE)");
+        // return $employees;
+
+        $budgetAllocationImport = new BudgetAllocationImport("LOE Data");
         Excel::import($budgetAllocationImport, $loe_file);
         $fundData = $budgetAllocationImport->fundData;
         
         
-        // return $fundData;
+        // return  ($fundData);
+
+
+        // return response()->json(['emp'=>count($employees),'loe'=>count($fundData)]);
         
         $service = new BudgetAllocationService();
         $processedData = $service->distributePayments($employees, $fundData,$submission_date,$doc_number,$external_doc_reference,$exchange_rate);
-        // return $processedData;
+        return $processedData;
         $export = new EmployeeExport($processedData);
 
         
         return Excel::download($export, 'distributed_salaries.csv');  //xlsx
     }
     
-    
+    public function generateTaxDeclarationTemplate(Request $request)
+    {
+
+        
+        // $request->validate([
+        //     'payroll_data' => 'required|mimes:xlsx',
+        //     "exchange_rate"=>"required", 
+        // 'date_picker'=> "required",]);
+
+
+        $payroll_file = $request->file('payroll_data');
+        $exchange_rate=  $request["exchange_rate"];
+        $submission_date=  $request["date_picker"];
+
+
+
+        // return "here";
+        $employeeImport = new EmployeeDataImportForTaxTemplate("Payroll Data",$exchange_rate,$submission_date);
+        Excel::import($employeeImport, $payroll_file);
+        $employees = $employeeImport->employees;
+        return $employees;
+        
+        
+    }
     // Method to export data to Excel
     public function export()
     {
